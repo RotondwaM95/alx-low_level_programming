4f1b61ef1cec5bb5622f32638d5565f29faa54b7
Rotondwa <rotondwamulondo@gmail.com>
2023-07-13 04:38:45 -0700

    first c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A program that print out a line
+ *it will return 0
+ */
+int main(void)
+{put("\"Programming is like building a multilingual puzzle");
+return (0);
+}
